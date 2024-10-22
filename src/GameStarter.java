0df9b1bd4d1c59d648decd673693e82b8d79e01f
NepSauce <greasyandmoist@gmail.com>
2024-10-21 21:22:11 -0300

    Added Base Files

---
+++ src/GameStarter.java
@@ -0,0 +1,12 @@
+/* @author(s): Zawad Atif, Nafisah Nubah
+ * Date Started: 2024-10-18
+ */
+
+
+import java.util.Scanner;
+
+public class GameStarter{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
+}
